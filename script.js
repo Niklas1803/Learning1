93e97ccf8cb9ae059db214f450263bb12e839ad9
Niklas1803 <100834797+Niklas1803@users.noreply.github.com>
2022-03-03 13:39:29 +0100

    java boxen

--- script.js
+++ script.js
@@ -2,5 +2,23 @@ function myFunction(){
 
     let output = "hello Javascript"
 
+        let a=5;
+
+        let b=3;
+
+        let c="hallo"
+
+        let d=a+b;
+
+
+
+
+
+
+
+
+
+
+
     document.getElementById("demo").innerHTML =output;
 }
